278a293bf0d587d7e5a89e3de33b3517b2a49673
Azman-Idrisi <idrisiazman@gmail.com>
2024-12-25 13:21:27 +0530

    Testimonial done

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,23 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
-import Testimonials from "./sections/Testimonials.jsx";
+import Testimonial from "./sections/Testimonial.jsx";
 import Download from "./sections/Download.jsx";
 import Footer from "./sections/Footer.jsx";
 
-
-
 const App = () => {
   return (
- <main className='overflow-hidden'>
-  <Header />
-  <Hero />  
-  <Features />
-  <Pricing />
-  <Faq />
-  <Testimonials />
-  <Download />
-  <Footer />
- </main>
-  )
-}
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonial />
+      <Download />
+      <Footer />
+    </main>
+  );
+};
 
-export default App
+export default App;
